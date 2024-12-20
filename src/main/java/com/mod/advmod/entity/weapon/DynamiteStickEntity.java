b46caf5588728212732af5caa743962a60701b9b
SupaHacka02 <mherericgyulumyan@gmail.com>
2024-12-20 15:13:41 -0400

    Dynamite Stick is impacted by gravity and has timed explosion

--- src/main/java/com/mod/advmod/entity/weapon/DynamiteStickEntity.java
+++ src/main/java/com/mod/advmod/entity/weapon/DynamiteStickEntity.java
@@ -4,12 +4,14 @@ import com.mod.advmod.entity.ModEntities;
 import com.mod.advmod.item.ModItems;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.BlockHitResult;
 
 public class DynamiteStickEntity extends ThrowableItemProjectile {
+
+    private int tickTillExplosion = 80;
 
     public DynamiteStickEntity(EntityType<? extends ThrowableItemProjectile> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
@@ -28,9 +30,18 @@ public class DynamiteStickEntity extends ThrowableItemProjectile {
     }
 
     @Override
-    protected void onHitBlock(BlockHitResult pResult) {
-        this.explode();
-        this.discard();
+    public void tick() {
+        this.applyGravity();
+        this.move(MoverType.SELF, this.getDeltaMovement());
+        this.setDeltaMovement(this.getDeltaMovement().scale(0.98));
+        if (this.onGround()) {
+            this.setDeltaMovement(this.getDeltaMovement().multiply(0.7, -0.5, 0.7));
+        }
+        if(this.tickTillExplosion <= 0) {
+            this.explode();
+            this.discard();
+        }
+        this.tickTillExplosion--;
     }
 
     @Override
