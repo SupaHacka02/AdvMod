9c3e07a59e542786888a5c0341f3be64a44a6b08
SupaHacka02 <mherericgyulumyan@gmail.com>
2024-12-28 22:33:52 -0400

    Changed Bunker Buster Logic

--- src/main/java/com/mod/advmod/entity/weapon/BunkerBusterEntity.java
+++ src/main/java/com/mod/advmod/entity/weapon/BunkerBusterEntity.java
@@ -2,17 +2,25 @@ package com.mod.advmod.entity.weapon;
 
 import com.mod.advmod.entity.ModEntities;
 import com.mod.advmod.item.ModItems;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MoverType;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.AirBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
 
 public class BunkerBusterEntity extends ThrowableItemProjectile {
 
-    private int blockBreaks = 5;
+    private int blockBreakTicks = 40;
+    private boolean start = false;
 
     public BunkerBusterEntity(EntityType<? extends ThrowableItemProjectile> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
@@ -26,23 +34,31 @@ public class BunkerBusterEntity extends ThrowableItemProjectile {
         super(ModEntities.BUNKER_BUSTER_ENTITY.get(), livingEntity, pLevel);
     }
 
-    @Override
-    protected void onHitBlock(BlockHitResult pResult) {
-        if(this.blockBreaks <= 0) {
-            this.discard();
-        }
-        this.blockBreaks--;
-        this.explode(4.25f);
-    }
-
-    @Override
-    protected void onHitEntity(EntityHitResult pResult) {
-        this.explode(6.5f);
-        this.discard();
-    }
-
     protected void explode(float explosionSize) {
         this.level().explode(this, this.getX(), this.getY(0.0625), this.getZ(), explosionSize, Level.ExplosionInteraction.TNT);
+    }
+
+    public void tick() {
+        this.applyGravity();
+        this.move(MoverType.SELF, this.getDeltaMovement());
+        this.setDeltaMovement(this.getDeltaMovement().scale(0.98));
+        HitResult hitresult = ProjectileUtil.getHitResultOnMoveVector(this, this::canHitEntity);
+        if(hitresult.getType() == HitResult.Type.ENTITY) {
+            this.explode(6.5f);
+            this.discard();
+        }
+        if(hitresult.getType() == HitResult.Type.BLOCK) {
+            if(!this.start) {
+                this.start = true;
+            }
+            this.explode(3.25f);
+        }
+        if(this.start) {
+            this.blockBreakTicks--;
+            if(this.blockBreakTicks <= 0) {
+                this.discard();
+            }
+        }
     }
 
     @Override
